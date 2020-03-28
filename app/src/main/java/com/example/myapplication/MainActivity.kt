d326505abfd7c99f2efe27520fcf2a4e812be0f2
Ashish Deshwal <deshwalashish.deshwal5@gmail.com>
2020-03-29 02:03:59 +0530

    Added dark mode

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,13 +2,26 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.CompoundButton
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
 import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+     darkModeSwitch.setOnCheckedChangeListener{ CompoundButton, isChecked ->
+         if (isChecked){
+             delegate.setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+         }else{
+             delegate.setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+         }
+     }
     }
 }
